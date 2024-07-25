128c10890913fa609948c31eb60f011cf9788261
Glorious Satria Dhamang Aji <glorioussatria@gmail.com>
2024-07-25 13:28:52 +0700

    addingt cloud instance

--- config/database.go
+++ config/database.go
@@ -1,51 +1,63 @@
 package config
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"net"
 
-	"github.com/spf13/viper"
-	"gorm.io/driver/mysql"
-	"gorm.io/gorm"
+	"cloud.google.com/go/cloudsqlconn"
+	"github.com/go-sql-driver/mysql"
 )
 
 type DatabaseConfig struct {
-	Host     string `mapstructure:"host"`
-	Port     string `mapstructure:"port"`
-	DBName   string `mapstructure:"dbname"`
-	Username string `mapstructure:"username"`
-	Password string `mapstructure:"password"`
+	Host                   string `mapstructure:"host"`
+	Port                   string `mapstructure:"port"`
+	DBName                 string `mapstructure:"dbname"`
+	Username               string `mapstructure:"username"`
+	Password               string `mapstructure:"password"`
+	InstanceConnectionName string `mapstructure:"instance_connection_name"`
+	UsePrivate             bool   `mapstructure:"use_private"`
 }
 
 type Config struct {
 	Database DatabaseConfig `mapstructure:"database"`
 }
 
-func InitDB() *gorm.DB {
+func InitDB() (*sql.DB, error) {
 	viper.SetConfigFile("prod.yaml")
 	err := viper.ReadInConfig()
 	if err != nil {
-		fmt.Println("Error reading config file :", err)
-		return nil
+		return nil, fmt.Errorf("Error reading config file: %w", err)
 	}
+
 	var config Config
 	err = viper.Unmarshal(&config)
 	if err != nil {
-		fmt.Println("Error unmarshalling config :", err)
-		return nil
+		return nil, fmt.Errorf("Error unmarshalling config: %w", err)
 	}
 
-	host := config.Database.Host
-	port := config.Database.Port
-	dbname := config.Database.DBName
-	username := config.Database.Username
-	password := config.Database.Password
-
-	dsn := username + ":" + password + "@tcp(" + host + ":" + port + ")/" + dbname + "?parseTime=true"
-	fmt.Println(dsn)
-	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{SkipDefaultTransaction: true, PrepareStmt: true})
-
+	d, err := cloudsqlconn.NewDialer(context.Background())
 	if err != nil {
-		panic("Can't connect to database")
+		return nil, fmt.Errorf("cloudsqlconn.NewDialer: %w", err)
 	}
-	return db
+
+	var opts []cloudsqlconn.DialOption
+	if config.Database.UsePrivate {
+		opts = append(opts, cloudsqlconn.WithPrivateIP())
+	}
+
+	mysql.RegisterDialContext("cloudsqlconn", func(ctx context.Context, addr string) (net.Conn, error) {
+		return d.Dial(ctx, config.Database.InstanceConnectionName, opts...)
+	})
+
+	dbURI := fmt.Sprintf("%s:%s@cloudsqlconn(localhost:%s)/%s?parseTime=true",
+		config.Database.Username, config.Database.Password, config.Database.Port, config.Database.DBName)
+
+	db, err := sql.Open("mysql", dbURI)
+	if err != nil {
+		return nil, fmt.Errorf("sql.Open: %w", err)
+	}
+
+	return db, nil
 }
--- config/migration.go
+++ config/migration.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-
 	"gorm.io/gorm"
 )
 
