e8f967f00dcd4e2f575ed320719cbf76a11fc5c9
Riyan Setiyadi Hidayatullah <setiyariyan19@gmail.com>
2024-07-28 01:09:19 +0700

    Update database.go

--- config/database.go
+++ config/database.go
@@ -56,7 +56,7 @@ func InitDB() (*gorm.DB, error) {
 	// 	return nil, fmt.Errorf("sql.Open: %w", err)
 	// }
 
-	dsn := "root:103c9c07e9b614c3@tcp(35.240.213.210:887)/kalorize?charset=utf8mb4&parseTime=True&loc=Local"
+	dsn := "root:103c9c07e9b614c3@tcp(35.240.213.210:887)/kalorize"
     db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
 
     fmt.Println("Successfully connected to the database!")
